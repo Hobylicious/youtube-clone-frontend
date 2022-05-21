c954bb6e4629b7c58e984deb7364a927bf1caf8a
Hobylicious <justiceguy88@gmail.com>
2022-05-21 12:18:13 -0400

    updated home page and app.js, also added routes/route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      App Holder Text
+
+      <HomePage />
+
     </div>
   );
 }
