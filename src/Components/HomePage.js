99e9433b23c2950d1ce78261d9ac29237f82a973
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-05-27 16:33:09 -0400

    I added a user profile, and the authentication button toggles depending on logged in state

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from './Header'
 import History from './History'
+import Profile from './UserProfile'
+import { useAuth0 } from "@auth0/auth0-react";
+import AuthenticationButton from './AuthButton'
+
+
 
 function HomePage() {
+    const {user} = useAuth0()
+    console.log({user})
     return (
         <>
             <Header />
             <div>Home Page</div>
             <History />
+            <AuthenticationButton/>
+            <Profile/>
         </>
     )
 }
