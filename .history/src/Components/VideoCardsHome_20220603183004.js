0354dadfe78d80ee3689f60ccc5f7b1bc1b595b9
ShindanoSingi <shindanosingi@sekur.com>
2022-06-03 21:31:59 -0500

    working front-end but css does not work

---
+++ .history/src/Components/VideoCardsHome_20220603183004.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const VideoCardsHome = ({ videos, suggestedVideos }) => {
+
+	//  console.log(videos)
+
+	function viewsFunc(num) {
+		if (num < 1000) return num;
+		else if ((num > 999) && (num < 1000000)) {
+			num = (num / 1000).toFixed(1);
+			return num + 'K';
+		}
+		else if ((num >= 1000000) && (num < 1000000000)) {
+			num = (num / 1000000).toFixed(1);
+			return num + 'M';
+		}
+
+		else if ((num > 1000000000) && (num < 1000000000000)) {
+			num = (num / 1000000000).toFixed(1);
+			return num + 'B';
+		}
+	}
+
+	// This component receives two objects: videoId has thumbnails and and videoIds..., and videos has statics information like views count, likes, dislikes...
+	const VideoCards = ({ videos, suggestedVodeos, viewsFunc }) => {
+
+		// If the user has not submitted a request for video, display a "Plese wait..." message.
+		if (videos.length < 1) {
+			return (
+				<div className="myModal modal">
+					<div className="modal-content">
+						<h1>Please wait...</h1>
+					</div>
+				</div>
+			)
+		}
+
+
+		// This return all clickable thumbnails cards.
+		else {
+			return (
+				<div className="cards-player" >
+					{videos.map((video, index) =>
+
+						video.type === 'video' && (
+							<Link className='link' to={`/videoPlayer/${video.id}`}>
+								< div key={video.id} className='single-card-player single-card'  >
+									<img className='image-player' src={video.thumbnails[0].url} height='60' alt='images' onClick={() => { document.getElementsByTagName('iframe')[0].src = `https://www.youtube.com/watch_popup?v=${video.id}` }} />
+									<div className="video-description">
+										<p className='title' onClick={() => { document.getElementsByTagName('iframe')[0].src = `https://www.youtube.com/watch_popup?v=${video.id}` }}>Title: {video.title}</p>
+										<div className='logo-cannel-views-posted-duration' >
+											<div className='channel-info' >
+												<div className='logo-channelName'>
+													<img className='channel-logo' width='25' height='25' src={`${video.author.avatars[0].url}`} alt="" />
+													<a className='channel-id' href={`https://www.youtube.com/channel/${video.author.channelID}`} target='_blank' rel='noreferrer'>{video.author.name}</a>
+												</div>
+												<div className='views-posted-duration' >
+													<p className='views'>Views: {viewsFunc(video.views)}</p>
+													<p className='views playing-video-uploadedAt'>Posted: {video.uploadedAt}</p>
+													<p className='views playing-video-uploadedAt'>Duration: {video.duration}</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</Link>
+						))}
+				</div>
+
+			)
+		}
+}
+
+
+export default VideoCardsHome;
+
